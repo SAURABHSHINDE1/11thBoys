e17171d932d659060d0fa19f9de2f99abd2506d8
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-01-13 17:18:15 +0530

    13 jan 2026

--- js11th/dom/dom.js
+++ js11th/dom/dom.js
@@ -14,11 +14,65 @@
 // image.style.height ="400px"
 // image.style.width="400px"
 
-let sname = document.getElementById("name")
-sname.textContent = "Name :- Ghansham"
+// let sname = document.getElementById("name")
+// sname.textContent = "Name :- Ghansham"
 
-let std = document.getElementById("std")
-std.textContent = "Std :-11th"
+// let std = document.getElementById("std")
+// std.textContent = "Std :-11th"
 
-let rollno = document.getElementById("rollno")
-rollno.textContent = "Roll No:- 167"
+// let rollno = document.getElementById("rollno")
+// rollno.textContent = "Roll No:- 167"
+
+
+// let container = document.querySelector(".container h1")
+
+// console.log(container)
+
+// let test = document.querySelector("#test")
+
+// console.log(test)
+
+// let heading  = document.querySelector("p")
+
+// console.log(heading)
+
+
+// create element
+
+// let box = document.querySelector(".box")
+
+// let heading  = document.createElement("h1")
+// heading.className="test"
+// heading.id="abc"
+// console.log(heading)
+// heading.textContent ="Hello DreamsGuider"
+
+// box.appendChild(heading)
+
+
+let body = document.querySelector("body")
+
+let card = document.createElement("div")
+card.className="card"
+body.appendChild(card)
+
+
+let upper = document.createElement("div")
+upper.className="upper"
+card.appendChild(upper)
+
+let img = document.createElement("img")
+img.src ="img.jfif"
+upper.appendChild(img)
+
+let lower = document.createElement("div")
+lower.className = "lower"
+card.appendChild(lower)
+
+let heading = document.createElement("h2")
+heading.textContent ="Name :- DreamsGuider"
+lower.appendChild(heading)
+
+let place = document.createElement("h2")
+place.textContent = "Location :- Loni"
+lower.appendChild(place)
