8f4a522237054ea86f28a00ce7a127b711c1466c
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-02-26 16:30:05 +0530

    26 feb 2026

--- js11th/dom/domDay3/script.js
+++ js11th/dom/domDay3/script.js
@@ -2,79 +2,154 @@
 let student = [
     {
         name :"Ram",
-        age : 20
+        age : 20,
+        image:"https://images.unsplash.com/photo-1761839257469-96c78a7c2dd3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
     },
 
      {
         name :"pratik",
-        age : 22
+        age : 22,
+        image:"https://plus.unsplash.com/premium_photo-1771774903177-633d046189f3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
     },
 
      {
         name :"ghansham",
-        age : 17
+        age : 17,
+        image:"https://plus.unsplash.com/premium_photo-1771141101661-8195069f6a22?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
     },
 
      {
         name :"Aditya",
-        age : 18
+        age : 18,
+        image:"https://plus.unsplash.com/premium_photo-1771141101661-8195069f6a22?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
     },
 
      {
         name :"Chaitanya",
-        age : 17
+        age : 17,
+        image:"https://images.unsplash.com/photo-1771583640401-f4ed27a3e4fe?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D"
     },
 
     {
         name :"saurabh",
-        age : 21
+        age : 21,
+        image:"https://images.unsplash.com/photo-1771600850804-da11efffee48?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"
     },
 
      {
         name :"rohit",
-        age : 21
+        age : 21,
+        image:"https://plus.unsplash.com/premium_photo-1771141101661-8195069f6a22?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
     },
 
      {
         name :"prashant",
-        age : 20
+        age : 20,
+        image:"https://images.unsplash.com/photo-1761839257469-96c78a7c2dd3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
     },
 
      {
         name :"abhishek",
-        age : 20
+        age : 20,
+        image:"https://plus.unsplash.com/premium_photo-1771774903177-633d046189f3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
     },
 
      {
         name :"vishal",
-        age : 20
+        age : 20,
+        image:"https://plus.unsplash.com/premium_photo-1771141101661-8195069f6a22?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
+    },
+
+
+     {
+        name :"Ram",
+        age : 20,
+        image:"https://images.unsplash.com/photo-1761839257469-96c78a7c2dd3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
+    },
+
+     {
+        name :"pratik",
+        age : 22,
+        image:"https://plus.unsplash.com/premium_photo-1771774903177-633d046189f3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
+    },
+
+     {
+        name :"ghansham",
+        age : 17,
+        image:"https://plus.unsplash.com/premium_photo-1771141101661-8195069f6a22?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
+    },
+
+     {
+        name :"Aditya",
+        age : 18,
+        image:"https://plus.unsplash.com/premium_photo-1771141101661-8195069f6a22?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
+    },
+
+     {
+        name :"Chaitanya",
+        age : 17,
+        image:"https://images.unsplash.com/photo-1771583640401-f4ed27a3e4fe?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        name :"saurabh",
+        age : 21,
+        image:"https://images.unsplash.com/photo-1771600850804-da11efffee48?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+     {
+        name :"rohit",
+        age : 21,
+        image:"https://plus.unsplash.com/premium_photo-1771141101661-8195069f6a22?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
+    },
+
+     {
+        name :"prashant",
+        age : 20,
+        image:"https://images.unsplash.com/photo-1761839257469-96c78a7c2dd3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
+    },
+
+     {
+        name :"abhishek",
+        age : 20,
+        image:"https://plus.unsplash.com/premium_photo-1771774903177-633d046189f3?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
+    },
+
+     {
+        name :"vishal",
+        age : 20,
+        image:"https://plus.unsplash.com/premium_photo-1771141101661-8195069f6a22?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
     },
     
 ]
 
 
-// console.log()
+// console.log(student[0].name)
 
-console.log(student.length)
+//  console.log(student[i].name)
+// console.log(student[i].age)
 
 let body = document.querySelector("body")
 
+for(let i =0 ; i <student.length ; i++){
+    
+    let card = document.createElement("div")
+    card.className = "card"
+    body.appendChild(card)
 
-for(let i = 0 ; i < student.length; i++){
+     let img = document.createElement("img")
+    img.src = student[i].image
+    card.appendChild(img)
 
-let card = document.createElement("div")
-card.className = "card"
-body.appendChild(card)
+    let sname = document.createElement("h2")
+    sname.textContent = `Name: ${student[i].name}`
+    card.appendChild(sname)
 
-let h2 = document.createElement("h2")
-h2.textContent = `Name : ${student[i].name}`
-card.appendChild(h2)
-
-let h3 = document.createElement("h3")
-h3.textContent = `Age : ${student[i].age}`
-card.appendChild(h3)
-
+    let age = document.createElement("h2")
+    age.textContent =  `Age: ${student[i].age}`
+    card.appendChild(age)
 }
+
 
 
 
---
+++ js11th/events/script.js
@@ -0,0 +1,35 @@
+
+
+// let btn = document.querySelector("#btn")
+
+// btn.addEventListener("click" , ()=>{
+//     window.location.href='test.html'
+// })
+
+let box = document.querySelector(".box")
+
+// box.addEventListener("click" , ()=>{
+
+//     box.style.backgroundColor = "teal"
+//     box.style.borderRadius = "50%"
+// })
+
+
+// box.addEventListener("dblclick" , ()=>{
+
+//     box.style.backgroundColor = "teal"
+//     box.style.borderRadius = "50%"
+// })
+
+
+// box.addEventListener("mouseenter" , ()=>{
+
+//     box.style.backgroundColor = "teal"
+//     box.style.borderRadius = "50%"
+// })
+
+// box.addEventListener("mouseleave" , ()=>{
+
+//     box.style.backgroundColor = "teal"
+//     box.style.borderRadius = "50%"
+// })
